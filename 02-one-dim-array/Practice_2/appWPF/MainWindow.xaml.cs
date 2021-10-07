bb67a56237a004d34861ce9f9f0fce68840b6a60
kirN-dev <novitskiy.kir@gmail.com>
2021-10-07 10:18:24 +0300

    Разметил интерфейс
    Добавил элемент таблицы и меню
    Добавил макет первого блока

--- 02-one-dim-array/Practice_2/appWPF/MainWindow.xaml.cs
+++ 02-one-dim-array/Practice_2/appWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace appWPF
 {
