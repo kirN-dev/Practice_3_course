03ab50a9221687ec9de020515b89ab2f26f88b27
kirN-dev <novitskiy.kir@gmail.com>
2021-10-07 10:16:21 +0300

    Выделил сохранения в отдельный класс

---
+++ 02-one-dim-array/Practice_2/LibraryArray/SaveSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace LibraryArray
+{
+    public class SaveSystem
+    {
+        private readonly BinaryFormatter _formatter = new BinaryFormatter();
+
+        public void Save(object data, string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                _formatter.Serialize(stream, data);
+            }
+        }
+
+        public object Open(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new ArgumentException();
+            }
+
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+               return _formatter.Deserialize(stream);
+            }
+        }
+    }
+}
